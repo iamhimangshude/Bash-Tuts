f05a560074f33a3a2af377cc82acdd0c7796a291
HimangshuDe <dehimangshu03@gmail.com>
2024-12-28 11:19:23 +0530

    Changes made ➕ / ➖
    AI now can retain chat history and will response accordingly!

--- py-ai.py
+++ py-ai.py
@@ -12,35 +12,52 @@ import time
 For automation, you can set alias in the customBashPrompt.sh file!
 """
 
-API_KEY = os.environ.get('GENAI_API_KEY', 'Not Found')
+# API_KEY = os.environ.get("GENAI_API_KEY", "Not Found")
+API_KEY = os.environ.get("DEHIM_GENAI_API_KEY", "Not Found")
 
 def strip_markdown(text):
-    return re.sub(r'\*\*(.*?)\*\*', r'\1', text).strip()
+    return re.sub(r"\*\*(.*?)\*\*", r"\1", text).strip()
 
 
-def chat_api_call(prompt: str = 'Draft a baby rhyme!'):
-    """Calls the Google Generative AI API to get a response"""
-    if prompt == '':
-        prompt = 'Draft a baby rhyme!'
-    genai.configure(api_key=API_KEY)
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(prompt)
-    strippedText = strip_markdown(response.text)
-    for text in strippedText:
-        print(text, end='', flush=True)
-        time.sleep(0.01)
+def chat_api_call(prompt: str = "Hello", history:list = []) -> list:
+    """
+    Calls the Google Generative AI API to get a response
     
+    `returns` the history as list
+    """
+    
+    if prompt == "":
+        prompt = "Hello"
+    
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
+        generation_config={
+            "temperature": 0.7,
+            "max_output_tokens": 2048,
+        },
+    )
+    
+    chat_session = model.start_chat(history=history)
+    response = chat_session.send_message(prompt)
+    strippedText = strip_markdown(response.text)
+    history.append({"role": "user", "parts": [prompt + "\n"]})
+    history.append({"role": "model", "parts": [strippedText + "\n"]})
+    for text in strippedText:
+        print(text, end="", flush=True)
+        time.sleep(0.01)
+    return history
 
 
 if __name__ == "__main__":
-    print("ASK to AI")
+    chat_history:list = None
+    print("Ask to AI")
     while True:
         try:
-            userPrompt = input("You: ")
+            userPrompt:str = input("You: ")
             print("AI:\n")
-            chat_api_call(userPrompt)
+            chat_history:list = chat_api_call(userPrompt, chat_history or [])
             print()
         except KeyboardInterrupt:
             print()
             break
-
